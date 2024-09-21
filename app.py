9cb8b5bba1a4e75b8f0995dd41a2b2a25336d1f9
ouryj <m.ouryjalloh@yahoo.com>
2024-09-21 15:48:04 -0400

    just saved all the apps

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 import streamlit as st
+import pandas as pd
+import plotly_express as px
 
-st.title("Welcome to My Streamlit Web App!")
-st.write("This is a basic web app with Streamlit.")
+#Reading csb file into pandas dataframe
+df = pd.read_csv('vehicles_us.csv')
+# adding a header to the app
+st.header('Vehicle Dataset Dashboard')
+
+#creating a histogram of the price column
+hist = px.histogram(df, x='price', title='Vehicle Price Distribution')
+
+# displaying the histogram with streamlit
+st.plotly_chart(hist)
+
+# creating a scatter plot for price vs odometer
+scatter_plot = px.scatter(df,x='price', y='odometer', title='Price Vs Mileage (Odometer)')
+
+#Display the scatter plot
+st.plotly_chart(scatter_plot)
+# adding a checkbox to toggle the scatter plot
+if st.checkbox('Show Scatter Plot: Price Vs Mileage'):
+    st.plotly_chart(scatter_plot)
+
+#adding a checkbox to toggle the histogram
+if st.checkbox('Show Histogram: Price Distribution'):
+    st.plotly_chart(hist)
+
+#finished now loading the app through the terminal
+
+
